06b236a7b5ed45821ede3e793d6160376f436dbd
Tranloi2k <tranloi20001007@gmail.com>
2023-03-30 15:06:39 +0700

    fix api url

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,10 @@ import { IndexRouter } from './controllers/v0/index.router';
 import bodyParser from 'body-parser';
 
 import { V0MODELS } from './controllers/v0/model.index';
+import { config } from './config/config';
 
+const c = config.dev;
+ 
 (async () => {
   await sequelize.addModels(V0MODELS);
   await sequelize.sync();
@@ -18,7 +21,7 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   //CORS Should be restricted
   app.use(function(req: any, res: { header: (arg0: string, arg1: string) => void; }, next: () => void) {
-    res.header("Access-Control-Allow-Origin", "http://a523dd94f76ae40e2a4d1a4e365f88de-1721778524.us-east-1.elb.amazonaws.com");
+    res.header("Access-Control-Allow-Origin", c.url);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -26,10 +29,7 @@ import { V0MODELS } from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter)
 
   // Root URI call
-  app.get( "/", async ( req: any, res: { send: (arg0: string) => void; } ) => {
-    res.send( "/api/v0/" );
-  } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
