9a4f2507197d3448c8f3eeaa6b8f3d71440a74cb
Tranloi2k <tranloi20001007@gmail.com>
2023-04-04 10:12:31 +0700

    fix api url

--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -45,7 +45,7 @@ router.get('/', async (req: Request, res: Response) => {
 
 // update a specific resource
 router.patch('/:id', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
         res.status(500).send("not implemented")
@@ -54,7 +54,7 @@ router.patch('/:id',
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
@@ -65,7 +65,7 @@ router.get('/signed-url/:fileName',
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
 router.post('/', 
-    requireAuth, 
+    // requireAuth, 
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;
